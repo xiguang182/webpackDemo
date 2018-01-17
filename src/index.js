96c17c71a72351f9df018040cdeea82b3af02107
xiguang182 <xiguang182@gmail.com>
2018-01-17 16:32:20 -0500

    0117
    Signed-off-by: xiguang182 <xiguang182@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import './style.css';
 const interact = require('interactjs');
 const locationModel = require('./location');
 var locations = {};
-// const yanxiaLocation = [
-//   {x: 251, y:103, range: 40},
-//   {x: 276, y:127, range: 40},
-//   {x: 319, y:114, range: 40},
-//   {x: 338, y:126, range: 40},
-//   {x: 194, y:156, range: 40},
-//   {x: 210, y:185, range: 40},
-//   {x: 138, y:360, range: 40},
-//   {x: 203, y:406, range: 40},
-//   {x: 382, y:213, range: 40},
-//   {x: 352, y:418, range: 40},
-// ];
+var hunts = {};
 window.onload = ()=>{
   let element = window.document.getElementById('yanxia');
   let snapRange = 40;
@@ -70,14 +59,32 @@ window.onload = ()=>{
       range: snapRange
     }
   })
-}
+  hunts.erle = {};
+  hunts.erle.element = window.document.getElementById('erle');
+  hunts.erle.map = 'fringes';
+  hunts.orcus = {};
+  hunts.orcus.element = window.document.getElementById('orcus');
+  hunts.orcus.map = 'fringes';
+  hunts.gaja = {};
+  hunts.gaja.element = window.document.getElementById('gajasura');
+  hunts.gaja.map = 'yanxia';
+  hunts.anga = {};
+  hunts.anga.element = window.document.getElementById('angada');
+  hunts.anga.map = 'yanxia';
+  
 
-interact('.yanxia')
+  for(let hunt in hunts){
+    delegation(hunts[hunt].element, locations[hunts[hunt].map]);
+  }
+  
+}
+function delegation(ele, loc){
+  let interac = interact(ele)
   .draggable({
     snap: {
       targets: [
         function (x, y) {
-          return locations.yanxia.reduce((acc, cur) =>{
+          return loc.reduce((acc, cur) =>{
             let distance1 = Math.pow((x-acc.x),2) + Math.pow((y-acc.y),2);
             let distance2 = Math.pow((x-cur.x),2) + Math.pow((y-cur.y),2);
             if(distance1 > distance2){
@@ -107,7 +114,56 @@ interact('.yanxia')
     // call this function on every dragend event
     onend: (event) =>{
     }
+  }).on('hold', function (event) {
+    event.currentTarget.classList.toggle('redBorder');
+    console.log(event.target)
+    interac.options.drag.enabled = !interac.options.drag.enabled;
+    // event.currentTarget.classList.remove('large');
   });
+  return interac;
+}
+
+// interact('.yanxia')
+//   .draggable({
+//     snap: {
+//       targets: [
+//         function (x, y) {
+//           return locations.yanxia.reduce((acc, cur) =>{
+//             let distance1 = Math.pow((x-acc.x),2) + Math.pow((y-acc.y),2);
+//             let distance2 = Math.pow((x-cur.x),2) + Math.pow((y-cur.y),2);
+//             if(distance1 > distance2){
+//               acc = cur;
+//             }
+//             return acc;
+//           })
+//       },],
+//       relativePoints: [
+//         { x: 0.5  , y: 1.2}    // and to the bottom-mid
+//       ],
+//       endOnly: true,
+//     },
+//     // enable inertial throwing
+//     inertia: true,
+//     // keep the element within the area of it's parent
+//     restrict: {
+//       restriction: "parent",
+//       endOnly: true,
+//       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+//     },
+//     // enable autoScroll
+//     autoScroll: true,
+
+//     // call this function on every dragmove event
+//     onmove: dragMoveListener,
+//     // call this function on every dragend event
+//     onend: (event) =>{
+//     }
+//   }).on('hold', function (event) {
+//     event.currentTarget.classList.toggle('redBorder');
+//     console.log(event.target)
+//     interac.options.drag.enabled = !interac.options.drag.enabled;
+//     // event.currentTarget.classList.remove('large');
+//   });
 
   interact('.fringes')
   .draggable({
@@ -293,6 +349,10 @@ interact('.yanxia')
     onend: (event) =>{
     }
   });
+
+
+
+
 
   function dragMoveListener (event) {
     var target = event.target,
